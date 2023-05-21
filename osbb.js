92cfd28571e867a702d4812dde342014e87267f5
Victor <assa.pvi@gmail.com>
2023-05-21 19:11:15 +0300

    change color

---
+++ osbb.js
@@ -0,0 +1,15 @@
+alert("открыть страницу?"); // приветсвие
+
+// let div = document.querySelector("div"); //что то с дивом?
+// div.innerText = "салют";
+
+
+// таблица усножения 
+// for (let x = 1; x < 10; x++) {
+//     console.log ("tabl" + x);
+//     for (let y = 1; y < 10; y++) {
+//         console.log (`${x}*${y}=${x*y} `);
+//     }
+//     console.log ('');
+// }
+    
